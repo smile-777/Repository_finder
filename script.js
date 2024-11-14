eb1be3a44da31d38b450d39ff5fd9c68a6ac373e
smile-777 <smile777usa@gmail.com>
2024-11-15 00:54:31 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,128 @@
-let response, responseJson, timerId;
 const form_input = document.querySelector(".input");
 const autocomplete = document.getElementById('autocomplete');
 const elements = document.querySelector('.elements');
 const repository_template = document.getElementById('repository-template').content;
 const rep_elem = repository_template.querySelector('.rep-elem');
+const preloader = document.querySelector(".preloader");
 const getDelay = 500;
+let controller = new AbortController();
+let signal = controller.signal;
+let reposMap;
+//localStorage.clear();
+
+if (localStorage.getItem("repositories")){
+  reposMap = new Map(Object.entries(JSON.parse(localStorage.getItem("repositories"))));
+  for (let repository of reposMap) {
+    renderCard(repository[1], repository[0]);
+  }
+} else {
+  reposMap = new Map();
+}
 
 searchRepository.oninput = async (event) => {
   event.preventDefault();
+  //controller.abort();
+  clearAutocomplete();
+  preloader.classList.add("visible");
   
   if (searchRepository.name.value.length) {
     try {
-      let debounceGetData = getData.bind(this, searchRepository.name.value);
-      await debounce(debounceGetData, getDelay).then(res => res());
-      responseJson = await response.json();
-    
-      while(autocomplete.firstChild){
-        autocomplete.removeChild(autocomplete.firstChild);
-      }
+      const response = await debounceGetData(searchRepository.name.value).then(res => res());
+      const responseJson = await response.json();
+      preloader.classList.remove("visible");
 
-      if (responseJson && searchRepository.name.value.length) {
-        for (let i = 0; i < 5; i++) {
-          const option = document.createElement("p");
-          option.classList.add("option");
-          option.textContent = responseJson["items"][i]["full_name"];
-          option.addEventListener("click", function() {
-            let clone = rep_elem.cloneNode(true);
-            clone.children[0].textContent = responseJson["items"][i]["name"];
-            clone.children[1].textContent = responseJson["items"][i]["owner"]["login"];
-            clone.children[2].textContent += responseJson["items"][i]["stargazers_count"];
-            let closeCross = clone.querySelector(".close");
-            closeCross.addEventListener("click", () => clone.remove(), { once: true });
-            elements.appendChild(clone);
-            form_input.value = '';
-          });
-          autocomplete.appendChild(option);
-        }
+      for (let i = 0; i < 1; i++) {
+        const optionName = responseJson["items"][i];
+        const option = document.createElement("p");
+        option.classList.add("option");
+        option.addEventListener("click", addOptionListener.bind(this, optionName));
+        postAutocomplete(option, optionName["full_name"]);
       }
     } catch(err) {
+        console.log(err);
         const option = document.createElement("p");
-        option.textContent = "такого репозитория нет";
-        autocomplete.appendChild(option);
+        if (err.message.includes("full_name")) {
+          postAutocomplete(option, "такого репозитория нет");
+        }
     };
   } else {
-      while(autocomplete.firstChild){
-      autocomplete.removeChild(autocomplete.firstChild);
-    }
+    clearAutocomplete();
   }
-}
+  preloader.classList.remove("visible");
+};
+
+
+//======== functions =========
 
 async function getData(val) {
-  response = await fetch(`https://api.github.com/search/repositories?q=${val}`, 
-    { method: 'GET', });
+  try {
+    return await fetch(`https://api.github.com/search/repositories?q=${val}`, 
+      { method: 'GET', 
+        signal: controller.signal,
+      });
+  } catch {}
 };
 
 const debounce = (fn, delay) => {
-      if (timerId) clearTimeout(timerId);
-      return new Promise(resolve => {
-        timerId = setTimeout(() => resolve(fn), delay);
-      });
+  let timerId;
+  return function(...args) {
+    if (timerId) clearTimeout(timerId);
+    return new Promise(resolve => {
+      let delayFn = fn.bind(this, ...args);
+      timerId = setTimeout(() => resolve(delayFn), delay);
+    });
+  }
 };
+
+const debounceGetData = debounce(getData, getDelay);
+
+function renderCard(value, repName) {
+  let clone = rep_elem.cloneNode(true);
+  clone.children[0].textContent = value["name"];
+
+  if (!value["owner"]) clone.children[1].textContent = value["login"];
+  else clone.children[1].textContent = value["owner"]["login"];
+
+  clone.children[2].textContent += value["stargazers_count"];
+  let closeCross = clone.querySelector(".close");
+
+  closeCross.addEventListener("click", () => {
+    reposMap.delete(repName);
+    writeToStorage(reposMap);
+    clone.remove();
+  });
+  elements.appendChild(clone);
+}
+
+function clearAutocomplete() {
+  while(autocomplete.firstChild){
+    autocomplete.removeChild(autocomplete.firstChild);
+  }
+}
+
+function writeToStorage(data) {
+  const map2obj = Object.fromEntries(data);
+  const obj2json = JSON.stringify(map2obj);
+  localStorage.setItem("repositories", obj2json);
+}
+
+function postAutocomplete(option, content) {
+  option.textContent = content;
+  autocomplete.appendChild(option);
+}
+
+function addOptionListener(option) {
+  const reposContent = {"name": option["name"],
+                        "login": option["owner"]["login"],
+                        "stargazers_count": option["stargazers_count"],
+                       };
+
+  const reposMapSize = reposMap.size;
+  reposMap.set(option["full_name"], reposContent);
+  writeToStorage(reposMap, option["full_name"]);
+
+  if (reposMapSize !== reposMap.size) {
+    renderCard(option, option["full_name"]);
+  }
+  form_input.value = '';
+}
